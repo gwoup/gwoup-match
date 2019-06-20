24c9f8c32d675a3ccd8444855b03f224d2c3502e
Alex Padalka <alex.com.ua@gmail.com>
2019-06-20 14:45:18 +0300

    update symbols set for pin generation

--- src/containers/QuizForm/index.js
+++ src/containers/QuizForm/index.js
@@ -134,7 +134,7 @@ class QuizForm extends Component {
 
   getRandomKey = (length) => {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789';
+    const characters = 'ABCDEFGHJKLMNPQRSTUVWXYZ123456789';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));

dd14fa2517107e4e9a3f211b3acd2d46ce18a16f
GwoupT <thomas@gwoup.com>
2020-03-19 22:58:59 +0000

    Last from Cloud9

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -69,7 +69,7 @@ export default class Signup extends Component {
         await Auth.signIn(this.state.email, this.state.password);
     
         this.props.userHasAuthenticated(true);
-        this.props.history.push("/");
+        this.props.history.push("/bematched");
       } catch (e) {
         alert(e.message);
         this.setState({ isLoading: false });
